44172553495fb1cfe95113e199bf45a4f391e808
Chandan Kumar <descentchandan@gmail.com>
2020-08-19 00:46:18 +0530

    layout code started

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout/Layout';
 
-function App() {
-  return (
+
+class App extends Component {
+  render(){
+    return (
     <div className="App">
-      
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
-  );
+  );}
 }
 
 export default App;
