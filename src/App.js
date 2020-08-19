9504511d39944c232a1e5b65b19d70d59637b09d
Chandan Kumar <descentchandan@gmail.com>
2020-08-19 14:35:34 +0530

    used css modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React,{Component} from 'react';
-import './App.css';
+import styles from './App.module.css';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
   render(){
     return (
-    <div className="App">
+    <div className={styles.App}>
       <Layout>
         <BurgerBuilder />
       </Layout>
