099790fb5878d567460c0060cc6596670e799bd1
Chandan Kumar <descentchandan@gmail.com>
2020-08-19 14:31:02 +0530

    started code in burgerbuilder.js

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+import Aux from "../../hoc/Auxiliary"
+
+class BurgerBuilder extends Component{
+
+
+    render(){
+        return (
+            <Aux>
+                <div>
+                    burger
+                </div>
+                <div>
+                    burger controls
+                </div>
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder
