5e4a143e0eec86c5f26507f5779bba04e0505648
Chandan Kumar <descentchandan@gmail.com>
2020-08-19 17:42:31 +0530

    ingredients added using state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react'
 import Burger from '../../components/Burger/Burger'
 import Aux from "../../hoc/Auxiliary"
 
-class BurgerBuilder extends Component{
-
-
-    render(){
-        return (
-            <Aux>
-                <Burger/>
-                <div>
-                    burger controls
-                </div>
-            </Aux>
-        )
-    }
+class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      bacon: 1,
+      meat: 1,
+      cheese: 2,
+      salad: 2,
+    },
+  };
+  render() {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <div>burger controls</div>
+      </Aux>
+    );
+  }
 }
 
 export default BurgerBuilder
