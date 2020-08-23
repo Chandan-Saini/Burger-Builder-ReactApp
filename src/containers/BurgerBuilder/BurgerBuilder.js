7a7060abd5c66232b29b385e1d887a358a606547
Chandan Kumar <descentchandan@gmail.com>
2020-08-23 14:02:57 +0530

    sending a post req with data to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,8 @@ import Aux from "../../hoc/Auxiliary/Auxiliary"
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
+
 
 const INGREDIENT_PRICES = {
   bacon: 20,
@@ -73,7 +75,25 @@ purchaseCancelHandler=()=>{
 }
 
 purchaseContinueHandler=()=>{
-  alert('You Continue...')
+  const order={
+    ingredients:this.state.ingredients,
+     price:this.state.totalPrice,
+     customer:{
+       name:"Chandan kr",
+       address:{
+         street:"demo street",
+         pincode:"201050",
+         country:"india"
+       },
+       email:"test@test.com"
+     },
+     deliveryMethod:'fastest'
+  }
+  axios.post('/orders.json',order).then(response=>{
+    console.log(response);
+  }).catch(error=>{
+    console.log(error);
+  })
 }
 
   render() {
