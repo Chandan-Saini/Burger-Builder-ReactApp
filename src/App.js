190490210a789a9cc01d85d1900e7b41a30dec92
Chandan Kumar <descentchandan@gmail.com>
2020-08-25 15:08:00 +0530

    included browserRouter and and some routes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import styles from './App.module.css';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
+import {Route,Switch} from 'react-router-dom'
+
 class App extends Component {
   render(){
     return (
-    <div className={styles.App}>
-      <Layout>
-        <BurgerBuilder />
-        <Checkout />
-      </Layout>
-    </div>
-  );}
+      <div className={styles.App}>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
+        </Layout>
+      </div>
+    );}
 }
 
 export default App;
