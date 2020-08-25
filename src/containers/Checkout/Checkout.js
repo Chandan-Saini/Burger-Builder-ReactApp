5b641a65f2d023e248b589b69b9309df72515752
Chandan Kumar <descentchandan@gmail.com>
2020-08-25 16:24:01 +0530

    making continue and back button work in chkoutSmry

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,23 +2,30 @@ import React, { Component } from 'react'
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
 
-class Checkout extends Component{
-     state={
-         ingredients:{
-             salad:1,
-             meat:1,
-             cheese:1,
-             bacon:1
-         }
-     }
-    render(){
+class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      cheese: 1,
+      bacon: 1,
+    },
+  };
 
-        return (
-            <div>
-                <CheckoutSummary ingredients={this.state.ingredients} />
-            </div>
-        )
-    }
+  checkoutCancelled=()=>{
+      this.props.history.goBack()
+}
+
+  checkoutContinued=()=>{
+      this.props.history.replace('/checkout/contact-data');
+  }
+  render() {
+    return (
+      <div>
+        <CheckoutSummary ingredients={this.state.ingredients} checkoutCancelled={this.checkoutCancelled} checkoutContinued={this.checkoutContinued} />
+      </div>
+    );
+  }
 }
 
 export default Checkout
