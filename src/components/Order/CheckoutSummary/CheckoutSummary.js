445f9a07b37930e514828a33bf794879923b13f6
Chandan Kumar <descentchandan@gmail.com>
2020-08-25 14:22:14 +0530

    corrected the places of checkout files

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Burger from'../'
+class CheckoutSummary extends Component{
+       
+    render(){
+
+        return ()
+    }
+}
+
+export default CheckoutSummary
