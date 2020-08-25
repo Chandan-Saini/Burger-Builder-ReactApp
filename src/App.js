4e9d54752c7481f61d0dd5639c1d898349598a6d
Chandan Kumar <descentchandan@gmail.com>
2020-08-25 14:46:07 +0530

    building the checkout code etc

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React,{Component} from 'react';
 import styles from './App.module.css';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-
+import Checkout from './containers/Checkout/Checkout'
 class App extends Component {
   render(){
     return (
     <div className={styles.App}>
       <Layout>
         <BurgerBuilder />
+        <Checkout />
       </Layout>
     </div>
   );}
--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
-import Burger from'../'
-class CheckoutSummary extends Component{
-       
-    render(){
+import Burger from'../../Burger/Burger'
+import Button from '../../UI/Button/Button'
+import classes from './CheckoutSummary.module.css'
 
-        return ()
-    }
+
+const CheckoutSummary= (props)=>{
+       
+        return (
+            <div className={classes.CheckoutSummary}>
+                 <h1>
+                     We hope it tastes well!
+                 </h1>
+                 <div style={{width:'300px', margin:'auto'}}>
+                     <Burger ingredients={props.ingredients} />
+                 </div>
+
+                 <Button btnType='Danger'>
+                       CANCEL
+                 </Button>
+                 <Button btnType='Success'>
+                       CONTINUE
+                 </Button>
+            </div>
+        )
 }
 
 export default CheckoutSummary
