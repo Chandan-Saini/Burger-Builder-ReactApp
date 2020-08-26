1a3d05335087042873fba8a9c4162524a17277c9
Chandan Kumar <descentchandan@gmail.com>
2020-08-26 13:53:55 +0530

    coaded the input files

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button  from '../../../components/UI/Button/Button'
 import Spinner from  '../../../components/UI/Spinner/Spinner'
 import classes from './ContactData.module.css'
 import axios from '../../../axios-orders'
+import Input from  '../../../components/UI/Input/Input'
 
 class ContactData extends Component{
     state={
@@ -43,26 +44,22 @@ class ContactData extends Component{
     render(){
       let form = (
         <form>
-          <input
-            className={classes.Input}
+          <Input inputtype="input"
             type="text"
             name="name"
             placeholder="Your name"
           />
-          <input
-            className={classes.Input}
+          <Input inputtype="input"
             type="email"
             name="email"
             placeholder="Your email"
           />
-          <input
-            className={classes.Input}
+          <Input inputtype="input"
             type="text"
             name="Street"
             placeholder="Your Street"
           />
-          <input
-            className={classes.Input}
+          <Input inputtype="input"
             type="text"
             name="postal"
             placeholder="Your postal code"
