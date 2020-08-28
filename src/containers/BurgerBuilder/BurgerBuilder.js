33ef50492b7a2b9b20c98062f9de6e9bd4de5884
Chandan Kumar <descentchandan@gmail.com>
2020-08-28 22:11:26 +0530

    making order now button work

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,6 @@ import {connect} from 'react-redux'
 
 class BurgerBuilder extends Component {
   state = {
-    purchasable:false,
     purchasing:false,
     loading:false,
     error:false
@@ -32,7 +31,7 @@ class BurgerBuilder extends Component {
     }).reduce((sum,el)=>{
       return sum+el
     },0)
-    this.setState({purchasable:sum>0})
+     return sum>0
   }
 
 purchaseHandler=()=>{
@@ -85,7 +84,7 @@ search:'?' + queryString})
               ingredientRemoved={this.props.onIngredientRemoved}
               disabled={disabledInfo}
               price={this.props.price}
-              purchasable={this.state.purchasable}
+              purchasable={this.updatePurchaseState(this.props.ings)}
               ordered={this.purchaseHandler}
             />
           </Aux>
