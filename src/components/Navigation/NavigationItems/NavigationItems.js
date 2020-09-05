263b6369fb14aef178a7fef69b1f40b94b702890
Chandan Kumar <descentchandan@gmail.com>
2020-09-05 14:28:35 +0530

    updating user is logedin or not

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,15 +2,19 @@ import React from "react";
 
 import classes from "./NavigationItems.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
+import { checkPropTypes } from "prop-types";
 
-const navigationItems = () => (
+const navigationItems = (props) => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/" exact>
       Burger Builder
     </NavigationItem>
     <NavigationItem link="/orders">Orders</NavigationItem>
-    <NavigationItem link="/auth">Authenticate</NavigationItem>
-  </ul>
+    {! props.isAuthenticated 
+    ? <NavigationItem link="/auth">Authenticate</NavigationItem>
+    : <NavigationItem link="/logout">Logout</NavigationItem>
+    }
+     </ul>
 );
 
 export default navigationItems;
